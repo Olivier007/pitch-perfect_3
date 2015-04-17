cc444d19d681ebbed7d2e57da31a54cf2316a73b
Olivier007 <fevryolivier@gmail.com>
2015-04-17 00:07:42 -0400

    Committed by Udacity

---
+++ Voob/Voob/RecordedAudio.swift
@@ -0,0 +1,35 @@
+//
+//  RecordedAudio.swift
+//  Voob
+//
+//  Created by Olivier on 4/15/15.
+//  Copyright (c) 2015 Layton. All rights reserved.
+//
+
+import Foundation
+class RecordedAudio: NSObject
+    
+   {
+    
+    var filePathUrl: NSURL!
+    var title: String!
+    
+    
+        init(filePathUrl: NSURL, title: String) {
+        self.filePathUrl = filePathUrl
+        self.title = title
+    }
+    
+    
+    
+    }
+    
+    
+    
+    
+
+
+
+
+
+
